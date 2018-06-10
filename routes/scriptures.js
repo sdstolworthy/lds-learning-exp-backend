32f78ab02d87cdd0539217b476240095fb3b3025
Spencer Stolworthy <sdstolworthy@gmail.com>
2018-06-10 17:53:36 -0600

    fixed routes

---
+++ routes/scriptures.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../models')
+
+router.get('/books', function(req, res, next) {
+  models.books.findAll({
+  }).then(books => {
+    res.send(books);
+  }).catch(() => {
+    res.status(502).send('There was an error connecting to the database')
+  })
+});
+router.get('/verses', function(req, res, next) {
+  models.verses.findAll({
+  }).then(verses => {
+    res.send(verses);
+  }).catch(() => {
+    res.status(502).send('There was an error connecting to the database')
+  })
+});
+router.get('/chapters', function(req, res, next) {
+  models.chapters.findAll({
+  }).then(chapters => {
+    res.send(chapters);
+  }).catch(() => {
+    res.status(502).send('There was an error connecting to the database')
+  })
+});
+router.get('/volumes', function(req, res, next) {
+  models.volumes.findAll({
+  }).then(volumes => {
+    res.send(volumes);
+  }).catch(() => {
+    res.status(502).send('There was an error connecting to the database')
+  })
+});
+
+module.exports = router
